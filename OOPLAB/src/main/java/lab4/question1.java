e6865ea3552ece823ae75e3f464fa577d9e0e4bf
ManeeshaYasanthi <maneewijewickrama@gmail.com>
2026-04-16 19:03:56 +0530

    lab4 q1 abstract parent created

---
+++ OOPLAB/src/main/java/lab4/question1.java
@@ -0,0 +1,41 @@
+package lab4;
+
+public class question1 {
+    
+public abstract class StaffMember {
+    
+   
+    private String fullName; 
+    private final String staffid; 
+    protected String department; 
+
+   
+    public StaffMember(String fullName, String staffid, String department) {
+        this.fullName = fullName;
+        this.staffid = staffid;
+        this.department = department;
+    }
+
+   
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getStaffid() {
+        return staffid;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public final void displayBasicDetails() {
+        System.out.println("Full Name: " + fullName);
+        System.out.println("Staff ID: " + staffid);
+        System.out.println("Department: " + department);
+    }
+
+  
+    public abstract double calculateMonthlyPayment();
+}  
+}

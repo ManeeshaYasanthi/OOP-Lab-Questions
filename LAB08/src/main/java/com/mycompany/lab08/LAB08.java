ba53dbf967577dfce0e99513e6878e2151485b45
ManeeshaYasanthi <maneewijewickrama@gmail.com>
2026-06-07 17:34:50 +0530

    dustbin

---
+++ LAB08/src/main/java/com/mycompany/lab08/LAB08.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab08;
+
+/**
+ *
+ * @author HP
+ */
+public class LAB08 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
